ea67923b43f96fc8416ae465f3b2899425330042
rhit-munrosl <munrosl@rose-hulman.edu>
2021-03-18 16:48:20 -0400

    Added files

--- Main.py
+++ Main.py
@@ -2,4 +2,15 @@ import json
 import flask
 from hashlib import sha256
 
-print("WHEEE")
+#transaction
+#block
+#proof method
+#hashing
+#server communications
+def main():
+    while(1==1):
+        num = input("Enter a command: ")
+        if(num == "Q"):
+            exit(1)
+
+main()
